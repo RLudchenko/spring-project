abe87cf4ddc689276f26f6c5612ec45a7d4abeb3
Rostyslav Ludchenko <skillet367@icloud.com>
2020-06-04 11:11:45 +0300

    fixed mvn errors

--- src/main/java/spring/intro/config/AppConfig.java
+++ src/main/java/spring/intro/config/AppConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.stereotype.Repository;
 import spring.intro.model.User;
 
 @Configuration
